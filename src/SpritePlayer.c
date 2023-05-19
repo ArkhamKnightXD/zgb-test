f57d77ed2be1659ad259b72016c24f22a28f719a
knight <karvin_jimenez@hotmail.com>
2023-05-18 20:15:00 -0400

    Agregada colisiones y animaciones a mi personaje

--- src/SpritePlayer.c
+++ src/SpritePlayer.c
@@ -2,21 +2,52 @@
 #include "Keys.h"
 #include "SpriteManager.h"
 
+//Las animaciones deben de ser definidas con array de Unsingned integer
+const UINT8 anim_idle[] = {1, 0}; //The first number indicates the number of frames
+
+//Debido a que el primer numero indica el numero de frames, mi animacion de walk 
+//tiene 2 frames, en los siguientes index indico la posicion de mis frames de animaciones que son el 1 y 2
+const UINT8 anim_walk[] = {2, 1, 2};
+
+//El collider de nuestro player se encuentra en el archivo player.gbr.meta, ahi podemos cambiar la posicion en X y Y del collider y tambien su width y height
+
 void START() {
 }
 
 void UPDATE() {
+
 	if(KEY_PRESSED(J_UP)) {
-		THIS->y--;
+
+		//Cuando ya agregamos collision debemos reemplazar esta linea por translateSprite
+		//THIS->y--;
+		//El primer parametro es This el segundo el valor en que aumentara X y el 3ero el valor que aumentara y
+		TranslateSprite(THIS, 0, -1);
+
+//Esta funcion se encarga de manejar las animaciones, le debo de enviar This (se refiere a 
+//la instancia de spritePlayer), luego indico las animaciones y al final a la cantidad de fps que se ejecutaran 
+//15 es lo ideal para juego de gb 30 es demasiado
+		SetSpriteAnim(THIS, anim_walk, 15);
 	} 
 	if(KEY_PRESSED(J_DOWN)) {
-		THIS->y++;
+		TranslateSprite(THIS, 0, 1);
+
+		SetSpriteAnim(THIS, anim_walk, 15);
 	}
 	if(KEY_PRESSED(J_LEFT)) {
-		THIS->x--;
+		TranslateSprite(THIS, -1, 0);
+
+		SetSpriteAnim(THIS, anim_walk, 15);
+
 	}
 	if(KEY_PRESSED(J_RIGHT)) {
-		THIS->x++;
+		TranslateSprite(THIS, 1, 0);
+
+		SetSpriteAnim(THIS, anim_walk, 15);
+	}
+
+//Cuando no se a presionado ningun boton, ejecuto el idle animation
+	if(keys == 0) {
+		SetSpriteAnim(THIS, anim_idle, 15);
 	}
 }
 
--- src/StateGame.c
+++ src/StateGame.c
@@ -6,9 +6,17 @@
 
 IMPORT_MAP(map);
 
+//Collisionable tiles are declared as an array of UINT8 ended with 0. 
+//If you open the file tiles.gbr you will see that we only have two tiles (the first one is empty and we don't want that to be collidable).
+// Declare the next array on StateGame.c
+UINT8 collision_tiles[] = {1, 0};
+
+
 void START() {
 	scroll_target = SpriteManagerAdd(SpritePlayer, 50, 50);
-	InitScroll(BANK(map), &map, 0, 0);
+
+	//Metodo encargado de hacer scroll, si queremos que haya collision debemos de pasarle el array collision tiles como 2do parametro y reemplazar el 0
+	InitScroll(BANK(map), &map, collision_tiles, 0);
 }
 
 void UPDATE() {
