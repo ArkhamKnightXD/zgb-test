5baa1a94fa44aaa74ecd693ea703fab817bfcb16
knight <karvin_jimenez@hotmail.com>
2023-05-18 23:04:49 -0400

    Refactorizando variables y funciones y documentando el codigo

--- src/SpritePlayer.c
+++ src/SpritePlayer.c
@@ -4,30 +4,18 @@
 #include "ZGBMain.h"
 
 //Las animaciones deben de ser definidas con array de Unsingned integer
-const UINT8 anim_idle[] = {1, 0}; //The first number indicates the number of frames
+const UINT8 idle_animation[] = {1, 0}; //The first number indicates the number of frames
 
 //Debido a que el primer numero indica el numero de frames, mi animacion de walk 
 //tiene 2 frames, en los siguientes index indico la posicion de mis frames de animaciones que son el 1 y 2
-const UINT8 anim_walk[] = {2, 1, 2};
+const UINT8 walking_animation[] = {2, 1, 2};
 
 //El collider de nuestro player se encuentra en el archivo player.gbr.meta, ahi podemos cambiar la posicion en X y Y del collider y tambien su width y height
 
 void START() {
 }
 
-void UPDATE() {
-
-	UINT8 i;
-	Sprite* spr;
-
-//Manejando la collision con mi enemigo
-	SPRITEMANAGER_ITERATE(i, spr) {
-		if(spr->type == SpriteEnemy) {
-			if(CheckCollision(THIS, spr)) {
-				SetState(StateGame);
-			}
-		}
-	}
+void PlayerMovement(){
 
 	if(KEY_PRESSED(J_UP)) {
 
@@ -39,29 +27,56 @@ void UPDATE() {
 //Esta funcion se encarga de manejar las animaciones, le debo de enviar This (se refiere a 
 //la instancia de spritePlayer), luego indico las animaciones y al final a la cantidad de fps que se ejecutaran 
 //15 es lo ideal para juego de gb 30 es demasiado
-		SetSpriteAnim(THIS, anim_walk, 15);
+		SetSpriteAnim(THIS, walking_animation, 15);
 	} 
 	if(KEY_PRESSED(J_DOWN)) {
 		TranslateSprite(THIS, 0, 1);
 
-		SetSpriteAnim(THIS, anim_walk, 15);
+		SetSpriteAnim(THIS, walking_animation, 15);
 	}
 	if(KEY_PRESSED(J_LEFT)) {
 		TranslateSprite(THIS, -1, 0);
 
-		SetSpriteAnim(THIS, anim_walk, 15);
+		SetSpriteAnim(THIS, walking_animation, 15);
 
 	}
 	if(KEY_PRESSED(J_RIGHT)) {
 		TranslateSprite(THIS, 1, 0);
 
-		SetSpriteAnim(THIS, anim_walk, 15);
+		SetSpriteAnim(THIS, walking_animation, 15);
 	}
 
 //Cuando no se a presionado ningun boton, ejecuto el idle animation
 	if(keys == 0) {
-		SetSpriteAnim(THIS, anim_idle, 15);
+		SetSpriteAnim(THIS, idle_animation, 15);
 	}
+
+}
+
+//Manejando la collision con mi enemigo, me parece que aqui busco todos los sprite y voy comparando 
+//a ver si el sprite del player collisiono con el sprite del enemigo
+void CheckCollisionWithEnemy(){
+	
+	UINT8 index;
+	Sprite* actualSprite;
+
+//SPRITEMANAGER_ITERATE is a macro that iterates through all the active sprites, if the sprite is of type SPRITE_ENEMY then we use the function 
+//CheckCollision to check if we are colliding with it (do it in this order). If that happens then SetState is called unloading 
+//the current state and loading the state passed as paramenter (even if it is the current one, like in this case)
+	SPRITEMANAGER_ITERATE(index, actualSprite) {
+		if(actualSprite->type == SpriteEnemy) {
+			if(CheckCollision(THIS, actualSprite)) {
+				SetState(StateGame);
+			}
+		}
+	}
+}
+
+void UPDATE() {
+
+	PlayerMovement();
+
+	CheckCollisionWithEnemy();
 }
 
 
